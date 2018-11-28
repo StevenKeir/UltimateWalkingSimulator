453e66cf9d05ef53a4a2f8bdd4d882c8d1d6f715
StevenKeir <39974047+StevenKeir@users.noreply.github.com>
2018-11-28 14:29:23 +0800

    fade

--- UltimateWalkingSimulator/Assets/FadeText.cs
+++ UltimateWalkingSimulator/Assets/FadeText.cs
@@ -8,174 +8,66 @@ public class FadeText : MonoBehaviour {
 
 
     public TMP_Text text;
-    public Image textBackGround;
+    //public Image textBackGround;
 
-    public bool faded;
-    public float TimeBetweenFade;
+    public bool fadingOut;
+    public bool fadingIn;
+    private float timeBetweenFade;
+    public float fadeTime;
 
     private void Start()
     {
-        faded = false;
-        text.alpha = 0f;
-        text.gameObject.SetActive(false);
-        //imageAlpha = textBackGround.color;
+        fadingOut = false;
+        fadingIn = false;
+
     }
     private void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.E))
+        if(timeBetweenFade > 1)
         {
-            text.gameObject.SetActive(true);
-            //StartCoroutine(Fade());
-            faded = true;
-            
+            timeBetweenFade = 1;
+            fadingOut = false;
+            fadingIn = false;
         }
-        if (faded == true) { TimeFade(); }
-    }
 
-    void TimeFade()
+        if(timeBetweenFade < 0)
+        {
+            timeBetweenFade = 0;
+            fadingOut = false;
+            fadingIn = false;
+        }
+
+        if (Input.GetKey(KeyCode.E) && fadingIn == false)
+        {
+            fadingOut = true;
+        }
+        if (Input.GetKey(KeyCode.R) && fadingOut == false)
+        {
+            fadingIn = true;
+        }
+
+        if (fadingOut == true && fadingIn == false)
+        {
+            TimeFadeOut();
+        }
+        if (fadingIn == true && fadingOut == false)
+        {
+            TimeFadeIn();
+        }
+
+    } 
+
+    void TimeFadeOut()
     {
-        TimeBetweenFade -= Time.deltaTime;
-        text.alpha = TimeBetweenFade;
+        timeBetweenFade -= Time.deltaTime / fadeTime;
+        text.alpha = timeBetweenFade;        
     }
 
-    IEnumerator Fade()
+    void TimeFadeIn()
     {
-        var imageAlpha = textBackGround.color;
-        textBackGround.color = imageAlpha;
-        if (!faded)
-        {
-            text.alpha = 0.90f;
-            imageAlpha.a = 0.90f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.80f;
-            imageAlpha.a = 0.80f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.70f;
-            imageAlpha.a = 0.70f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.60f;
-            imageAlpha.a = 0.60f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.50f;
-            imageAlpha.a = 0.50f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.40f;
-            imageAlpha.a = 0.40f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.30f;
-            imageAlpha.a= 0.30f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.20f;
-            imageAlpha.a = 0.20f;
-            yield return new WaitForSeconds(0.01f);            
-            text.alpha = 0.10f;
-            imageAlpha.a = 0.10f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.00f;
-            imageAlpha.a = 0.00f;
-            yield return new WaitForSeconds(TimeBetweenFade);
-            faded = true;
-           
-            text.alpha = 0.00f;
-            imageAlpha.a = 0.00f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.20f;
-            imageAlpha.a = 0.20f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.30f;
-            imageAlpha.a = 0.30f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.40f;
-            imageAlpha.a = 0.40f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.50f;
-            imageAlpha.a = 0.50f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.60f;
-            imageAlpha.a = 0.60f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.70f;
-            imageAlpha.a = 0.70f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.80f;
-            imageAlpha.a = 0.80f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.90f;
-            imageAlpha.a = 0.90f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 1f;
-            imageAlpha.a = 1f;
-            faded = false;
-        }
-        else
-        {
-            text.alpha = 0.00f;
-            imageAlpha.a = 0.00f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.20f;
-            imageAlpha.a = 0.20f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.30f;
-            imageAlpha.a = 0.30f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.40f;
-            imageAlpha.a = 0.40f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.50f;
-            imageAlpha.a = 0.50f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.60f;
-            imageAlpha.a = 0.60f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.70f;
-            imageAlpha.a = 0.70f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.80f;
-            imageAlpha.a = 0.80f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.90f;
-            imageAlpha.a = 0.90f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 1f;
-            imageAlpha.a = 1f;
-            yield return new WaitForSeconds(TimeBetweenFade);
-            faded = false;
-           
-            text.alpha = 0.90f;
-            imageAlpha.a = 0.90f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.80f;
-            imageAlpha.a = 0.80f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.70f;
-            imageAlpha.a = 0.70f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.60f;
-            imageAlpha.a = 0.60f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.50f;
-            imageAlpha.a = 0.50f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.40f;
-            imageAlpha.a = 0.40f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.30f;
-            imageAlpha.a = 0.30f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.20f;
-            imageAlpha.a = 0.20f;
-            yield return new WaitForSeconds(0.01f);            
-            text.alpha = 0.10f;
-            imageAlpha.a = 0.10f;
-            yield return new WaitForSeconds(0.01f);
-            text.alpha = 0.00f;
-            imageAlpha.a = 0.00f;
-            faded = true;
-            
-            
-        }
-
+        timeBetweenFade += Time.deltaTime / fadeTime;
+        text.alpha = timeBetweenFade;
     }
+
 
 }

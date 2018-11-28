38c3a557cf667f2b72de6a3f8f3bf2c7dde9266c
NathanBorona <39974058+NathanBorona@users.noreply.github.com>
2018-11-28 16:17:16 +0800

    Nathan

---
+++ UltimateWalkingSimulator/Assets/Scripts/FootstepsRandom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepsRandom : MonoBehaviour {
+
+    public PlayerMovement myPlayer;
+
+    public AudioSource[] footsteps;
+    public int howManySteps;
+    public int stepNumber;
+    public float timer;
+    public float stepTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Horizontal") >= 0.01f || Input.GetAxis("Horizontal") <= -0.01f)
+        {
+            stepNumber = Random.Range(0, howManySteps);
+            timer += Time.deltaTime;
+            if (timer >= stepTime)
+            {
+                footsteps[stepNumber].Play();
+                timer = 0;
+            }
+        }
+	}
+}

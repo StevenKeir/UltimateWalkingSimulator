84bd8bfa9d3479c697bb65c5c197434123a121f4
StevenKeir <39974047+StevenKeir@users.noreply.github.com>
2018-11-29 17:39:22 +0800

    MainMenu

---
+++ UltimateWalkingSimulator/Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public int sceneToLoad;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
